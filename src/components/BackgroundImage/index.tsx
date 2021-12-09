aa459b0d9116e6860fdfe691adc378f73071e410
Jose Manuel Tuberquia <j.tuberquia@mobiera.com>
2021-12-09 07:38:45 -0500

    Create Starter Component, BackgroundImage

---
+++ src/components/BackgroundImage/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+interface Props {
+  title: string;
+  srcImg: string;
+}
+const BackgroundImage = ({ title, srcImg }: Props) => (
+  <div className="container_background_img">
+    <img src={srcImg} alt={`${title}`} className="background_img" />
+    <p className="title">{title}</p>
+  </div>
+);
+
+export default BackgroundImage;
