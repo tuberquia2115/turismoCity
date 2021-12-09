22124794d57a1031205f384f925bf7d5ade05ff3
Jose Manuel Tuberquia <j.tuberquia@mobiera.com>
2021-12-09 07:42:00 -0500

    Create the Logo component

---
+++ src/components/UI/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import imgLogo from '../../../assets/logo.png';
+
+import './styles.css';
+
+const Logo = () => (
+  <div className="container_logo">
+    <img src={imgLogo} alt="logo-turismo-city" className="logo" />
+  </div>
+);
+
+export default Logo;
